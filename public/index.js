7d5f716f1928ae82ce8db832897357ca897cb71d
Dwight <dwightdoane@me.com>
2023-07-03 15:10:09 -0400

    is this how i setup analytics?

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
-import "@vercel/analytics";
+import { inject } from "@vercel/analytics";
 
 export default {
   load: async function (elmLoaded) {
     const app = await elmLoaded;
+    inject();
     // console.log("App loaded", app);
   },
   flags: function () {

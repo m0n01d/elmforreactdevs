9315b06263ed69daa765a6ed22d3bce467403fc8
Dwight <dwightdoane@me.com>
2023-07-12 22:15:09 -0400

    wip

--- index.ts
+++ index.ts
@@ -9,11 +9,9 @@ var process = process || undefined;
 const config: ElmPagesInit = {
   load: async function (elmLoaded) {
     await elmLoaded;
-    process &&
-      inject({
-        mode:
-          typeof process?.env.VERCEL_ENV != "undefined" ? "production" : "dev",
-      });
+    inject({
+      mode: typeof process.env.VERCEL_ENV != "undefined" ? "production" : "dev",
+    });
   },
   flags: function () {
     return "You can decode this in Shared.elm using Json.Decode.string!";

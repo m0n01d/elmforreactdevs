0d6d7fa778fe630ea6915108e02ba356a0af0a71
Dwight <dwightdoane@me.com>
2023-07-12 22:53:23 -0400

    dev env

--- index.ts
+++ index.ts
@@ -7,9 +7,8 @@ type ElmPagesInit = {
 const config: ElmPagesInit = {
   load: async function (elmLoaded) {
     await elmLoaded;
-    document.body.classList.add(process.env.VERCEL_ENV);
     inject({
-      mode: typeof process.env.VERCEL_ENV != "undefined" ? "production" : "dev",
+      mode: "production",
     });
   },
   flags: function () {
